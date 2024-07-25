95e5c2b4f482f4895e307d4dbd75c15e9b0333aa
angellicamarta <162157769+angellicamarta@users.noreply.github.com>
2024-07-25 17:46:46 +0100

    prepare_believe_data.R

--- prepare_believe_data.R
+++ prepare_believe_data.R
@@ -14,13 +14,19 @@ quantitative_covariates <- read.table("/home/am3194/rds/hpc-work/gcta_believe/ph
 study_discrete <- read.table("/home/am3194/rds/hpc-work/gcta_believe/pheno_data/study_discrete.env", header = FALSE, col.names = c("FID", "IID", "Study"))
 
 # Prepare phenotype and covariate matrices
-y <- as.factor(phenotype$T2D_status)
+y <- phenotype$T2D_status
 
-# Remove missing data coded as -9
-y <- y[y != "-9"]
-X <- X[y != "-9", ]
+# Remove missing data coded as -9 and corresponding rows in covariates
+valid_indices <- y != "-9"
+y <- y[valid_indices]
+vigorous_activity <- vigorous_activity[valid_indices, ]
+quantitative_covariates <- quantitative_covariates[valid_indices, ]
+categorical_covariates <- categorical_covariates[valid_indices, ]
+albumin_discrete <- albumin_discrete[valid_indices, ]
+study_discrete <- study_discrete[valid_indices, ]
 
 # Convert necessary variables to factors
+y <- as.factor(y)
 PA <- as.numeric(vigorous_activity$Vigorous10MinsActivity)
 BMI <- as.numeric(quantitative_covariates$BMI)
 Age <- as.numeric(quantitative_covariates$Age)
