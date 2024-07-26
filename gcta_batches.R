4372efe7f78288f909f39dfd1cabceca398a82ab
angellicamarta <162157769+angellicamarta@users.noreply.github.com>
2024-07-27 00:27:00 +0100

    Update gcta_batches.R

--- gcta_batches.R
+++ gcta_batches.R
@@ -30,80 +30,65 @@ study_discrete <- study_discrete[valid_indices, ]
 # Use the valid indices to filter the GRM
 GRM <- GRM[valid_indices, valid_indices]
 
-# Split indices for 5 batches
-n_samples <- length(valid_indices)
-batch_size <- floor(n_samples / 5)
+# Define batch sizes
+batch_size <- 10000
+last_batch_size <- 13717
 
-# Indices for each batch
+# Batch indices
 indices_batch_1 <- 1:batch_size
 indices_batch_2 <- (batch_size + 1):(2 * batch_size)
 indices_batch_3 <- (2 * batch_size + 1):(3 * batch_size)
 indices_batch_4 <- (3 * batch_size + 1):(4 * batch_size)
-indices_batch_5 <- (4 * batch_size + 1):n_samples
-
-# Adjust the last batch to include any remaining samples
-if (max(indices_batch_5) > n_samples) {
-  indices_batch_5 <- (4 * batch_size + 1):n_samples
-}
-
-# Batch 1
-GRM_batch_1 <- GRM[indices_batch_1, indices_batch_1]
-ids_batch_1 <- phenotype[indices_batch_1, c("FID", "IID")]
-
-# Convert GRM matrix into vector form
-GRM_batch_1_vec <- as.vector(GRM_batch_1)
+indices_batch_5 <- (4 * batch_size + 1):(5 * batch_size)
+indices_batch_6 <- (5 * batch_size + 1):(5 * batch_size + last_batch_size)
 
 # Write GRM files for Batch 1
+GRM_batch_1 <- GRM[indices_batch_1, indices_batch_1]
+ids_batch_1 <- phenotype[indices_batch_1, c("FID", "IID")]
+GRM_batch_1_vec <- as.vector(GRM_batch_1)
 write.table(ids_batch_1, file = "GRM_batch_1.grm.id", quote = FALSE, row.names = FALSE, col.names = FALSE)
 writeBin(GRM_batch_1_vec, "GRM_batch_1.grm.bin", size = 4)
 writeBin(rep(1, length(GRM_batch_1_vec)), "GRM_batch_1.grm.N.bin", size = 4)
 
-# Batch 2
+# Write GRM files for Batch 2
 GRM_batch_2 <- GRM[indices_batch_2, indices_batch_2]
 ids_batch_2 <- phenotype[indices_batch_2, c("FID", "IID")]
-
-# Convert GRM matrix into vector form
 GRM_batch_2_vec <- as.vector(GRM_batch_2)
-
-# Write GRM files for Batch 2
 write.table(ids_batch_2, file = "GRM_batch_2.grm.id", quote = FALSE, row.names = FALSE, col.names = FALSE)
 writeBin(GRM_batch_2_vec, "GRM_batch_2.grm.bin", size = 4)
 writeBin(rep(1, length(GRM_batch_2_vec)), "GRM_batch_2.grm.N.bin", size = 4)
 
-# Batch 3
+# Write GRM files for Batch 3
 GRM_batch_3 <- GRM[indices_batch_3, indices_batch_3]
 ids_batch_3 <- phenotype[indices_batch_3, c("FID", "IID")]
-
-# Convert GRM matrix into vector form
 GRM_batch_3_vec <- as.vector(GRM_batch_3)
-
-# Write GRM files for Batch 3
 write.table(ids_batch_3, file = "GRM_batch_3.grm.id", quote = FALSE, row.names = FALSE, col.names = FALSE)
 writeBin(GRM_batch_3_vec, "GRM_batch_3.grm.bin", size = 4)
 writeBin(rep(1, length(GRM_batch_3_vec)), "GRM_batch_3.grm.N.bin", size = 4)
 
-# Batch 4
+# Write GRM files for Batch 4
 GRM_batch_4 <- GRM[indices_batch_4, indices_batch_4]
 ids_batch_4 <- phenotype[indices_batch_4, c("FID", "IID")]
-
-# Convert GRM matrix into vector form
 GRM_batch_4_vec <- as.vector(GRM_batch_4)
-
-# Write GRM files for Batch 4
 write.table(ids_batch_4, file = "GRM_batch_4.grm.id", quote = FALSE, row.names = FALSE, col.names = FALSE)
 writeBin(GRM_batch_4_vec, "GRM_batch_4.grm.bin", size = 4)
 writeBin(rep(1, length(GRM_batch_4_vec)), "GRM_batch_4.grm.N.bin", size = 4)
 
-# Batch 5
+# Write GRM files for Batch 5
 GRM_batch_5 <- GRM[indices_batch_5, indices_batch_5]
 ids_batch_5 <- phenotype[indices_batch_5, c("FID", "IID")]
-
-# Convert GRM matrix into vector form
 GRM_batch_5_vec <- as.vector(GRM_batch_5)
-
-# Write GRM files for Batch 5
 write.table(ids_batch_5, file = "GRM_batch_5.grm.id", quote = FALSE, row.names = FALSE, col.names = FALSE)
 writeBin(GRM_batch_5_vec, "GRM_batch_5.grm.bin", size = 4)
 writeBin(rep(1, length(GRM_batch_5_vec)), "GRM_batch_5.grm.N.bin", size = 4)
+
+# Write GRM files for Batch 6
+GRM_batch_6 <- GRM[indices_batch_6, indices_batch_6]
+ids_batch_6 <- phenotype[indices_batch_6, c("FID", "IID")]
+GRM_batch_6_vec <- as.vector(GRM_batch_6)
+write.table(ids_batch_6, file = "GRM_batch_6.grm.id", quote = FALSE, row.names = FALSE, col.names = FALSE)
+writeBin(GRM_batch_6_vec, "GRM_batch_6.grm.bin", size = 4)
+writeBin(rep(1, length(GRM_batch_6_vec)), "GRM_batch_6.grm.N.bin", size = 4)
+
 
                        
