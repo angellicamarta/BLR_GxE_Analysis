943bc3d7e09cb49c554e7aa9b9693f5fcf632149
angellicamarta <162157769+angellicamarta@users.noreply.github.com>
2024-07-27 01:23:46 +0100

    Update gcta_batches.R

--- gcta_batches.R
+++ gcta_batches.R
@@ -42,33 +42,16 @@ indices_batch_4 <- (3 * batch_size + 1):(4 * batch_size)
 indices_batch_5 <- (4 * batch_size + 1):(5 * batch_size)
 indices_batch_6 <- (5 * batch_size + 1):(5 * batch_size + last_batch_size)
 
-# Helper function to process each batch
+# Create function to process each batch
 process_batch <- function(indices, batch_number) {
-  # Filter GRM and data for the batch
   GRM_batch <- GRM[indices, indices]
-  phenotype_batch <- phenotype[indices, ]
-  albumin_discrete_batch <- albumin_discrete[indices, ]
-  vigorous_activity_batch <- vigorous_activity[indices, ]
-  categorical_covariates_batch <- categorical_covariates[indices, ]
-  quantitative_covariates_batch <- quantitative_covariates[indices, ]
-  study_discrete_batch <- study_discrete[indices, ]
-
-  # Create IDs for the batch
-  ids_batch <- phenotype_batch[, c("FID", "IID")]
-
+  ids_batch <- phenotype[indices, c("FID", "IID")]
+  
   # Convert GRM matrix into vector form
   GRM_batch_vec <- as.vector(GRM_batch)
-
+  
   # Write GRM files for the batch using WriteGRMBin
-  WriteGRMBin(prefix = paste0("GRM_batch_", batch_number), grm = GRM_batch_vec, N = length(indices), id = ids_batch, size = 4)
-
-  # Write data files for the batch
-  write.table(phenotype_batch, file = paste0("phenotype_batch_", batch_number, ".pheno"), row.names = FALSE, col.names = FALSE, quote = FALSE)
-  write.table(albumin_discrete_batch, file = paste0("albumin_batch_", batch_number, ".env"), row.names = FALSE, col.names = FALSE, quote = FALSE)
-  write.table(vigorous_activity_batch, file = paste0("vigorous_batch_", batch_number, ".env"), row.names = FALSE, col.names = FALSE, quote = FALSE)
-  write.table(categorical_covariates_batch, file = paste0("categorical_covariates_batch_", batch_number, ".covar"), row.names = FALSE, col.names = FALSE, quote = FALSE)
-  write.table(quantitative_covariates_batch, file = paste0("quantitative_covariates_batch_", batch_number, ".qcovar"), row.names = FALSE, col.names = FALSE, quote = FALSE)
-  write.table(study_discrete_batch, file = paste0("study_batch_", batch_number, ".env"), row.names = FALSE, col.names = FALSE, quote = FALSE)
+  WriteGRMBin(prefix = paste0("GRM_batch_", batch_number), grm = GRM_batch_vec, N = nrow(GRM_batch), id = ids_batch, size = 4)
 }
 
 # Process each batch
@@ -78,4 +61,40 @@ process_batch(indices_batch_3, 3)
 process_batch(indices_batch_4, 4)
 process_batch(indices_batch_5, 5)
 process_batch(indices_batch_6, 6)
+
+# Ensure environmental variables and covariates are split correctly for each batch
+write.table(albumin_discrete[indices_batch_1, ], file = "albumin_batch_1.env", quote = FALSE, row.names = FALSE, col.names = FALSE)
+write.table(albumin_discrete[indices_batch_2, ], file = "albumin_batch_2.env", quote = FALSE, row.names = FALSE, col.names = FALSE)
+write.table(albumin_discrete[indices_batch_3, ], file = "albumin_batch_3.env", quote = FALSE, row.names = FALSE, col.names = FALSE)
+write.table(albumin_discrete[indices_batch_4, ], file = "albumin_batch_4.env", quote = FALSE, row.names = FALSE, col.names = FALSE)
+write.table(albumin_discrete[indices_batch_5, ], file = "albumin_batch_5.env", quote = FALSE, row.names = FALSE, col.names = FALSE)
+write.table(albumin_discrete[indices_batch_6, ], file = "albumin_batch_6.env", quote = FALSE, row.names = FALSE, col.names = FALSE)
+
+write.table(vigorous_activity[indices_batch_1, ], file = "vigorous_batch_1.env", quote = FALSE, row.names = FALSE, col.names = FALSE)
+write.table(vigorous_activity[indices_batch_2, ], file = "vigorous_batch_2.env", quote = FALSE, row.names = FALSE, col.names = FALSE)
+write.table(vigorous_activity[indices_batch_3, ], file = "vigorous_batch_3.env", quote = FALSE, row.names = FALSE, col.names = FALSE)
+write.table(vigorous_activity[indices_batch_4, ], file = "vigorous_batch_4.env", quote = FALSE, row.names = FALSE, col.names = FALSE)
+write.table(vigorous_activity[indices_batch_5, ], file = "vigorous_batch_5.env", quote = FALSE, row.names = FALSE, col.names = FALSE)
+write.table(vigorous_activity[indices_batch_6, ], file = "vigorous_batch_6.env", quote = FALSE, row.names = FALSE, col.names = FALSE)
+
+write.table(categorical_covariates[indices_batch_1, ], file = "categorical_covariates_batch_1.covar", quote = FALSE, row.names = FALSE, col.names = FALSE)
+write.table(categorical_covariates[indices_batch_2, ], file = "categorical_covariates_batch_2.covar", quote = FALSE, row.names = FALSE, col.names = FALSE)
+write.table(categorical_covariates[indices_batch_3, ], file = "categorical_covariates_batch_3.covar", quote = FALSE, row.names = FALSE, col.names = FALSE)
+write.table(categorical_covariates[indices_batch_4, ], file = "categorical_covariates_batch_4.covar", quote = FALSE, row.names = FALSE, col.names = FALSE)
+write.table(categorical_covariates[indices_batch_5, ], file = "categorical_covariates_batch_5.covar", quote = FALSE, row.names = FALSE, col.names = FALSE)
+write.table(categorical_covariates[indices_batch_6, ], file = "categorical_covariates_batch_6.covar", quote = FALSE, row.names = FALSE, col.names = FALSE)
+
+write.table(quantitative_covariates[indices_batch_1, ], file = "quantitative_covariates_batch_1.qcovar", quote = FALSE, row.names = FALSE, col.names = FALSE)
+write.table(quantitative_covariates[indices_batch_2, ], file = "quantitative_covariates_batch_2.qcovar", quote = FALSE, row.names = FALSE, col.names = FALSE)
+write.table(quantitative_covariates[indices_batch_3, ], file = "quantitative_covariates_batch_3.qcovar", quote = FALSE, row.names = FALSE, col.names = FALSE)
+write.table(quantitative_covariates[indices_batch_4, ], file = "quantitative_covariates_batch_4.qcovar", quote = FALSE, row.names = FALSE, col.names = FALSE)
+write.table(quantitative_covariates[indices_batch_5, ], file = "quantitative_covariates_batch_5.qcovar", quote = FALSE, row.names = FALSE, col.names = FALSE)
+write.table(quantitative_covariates[indices_batch_6, ], file = "quantitative_covariates_batch_6.qcovar", quote = FALSE, row.names = FALSE, col.names = FALSE)
+
+write.table(study_discrete[indices_batch_1, ], file = "study_batch_1.env", quote = FALSE, row.names = FALSE, col.names = FALSE)
+write.table(study_discrete[indices_batch_2, ], file = "study_batch_2.env", quote = FALSE, row.names = FALSE, col.names = FALSE)
+write.table(study_discrete[indices_batch_3, ], file = "study_batch_3.env", quote = FALSE, row.names = FALSE, col.names = FALSE)
+write.table(study_discrete[indices_batch_4, ], file = "study_batch_4.env", quote = FALSE, row.names = FALSE, col.names = FALSE)
+write.table(study_discrete[indices_batch_5, ], file = "study_batch_5.env", quote = FALSE, row.names = FALSE, col.names = FALSE)
+write.table(study_discrete[indices_batch_6, ], file = "study_batch_6.env", quote = FALSE, row.names = FALSE, col.names = FALSE)
 
